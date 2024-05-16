d938507e72729d3d66b9dadbac6cf6c4614f1db8
Sunair Anwar <102017033+sunairahmed99@users.noreply.github.com>
2024-05-16 12:28:32 +0500

    fgff
    fddf

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -5,22 +5,24 @@ import emailjs from "@emailjs/browser";
 const Join = () => {
   const form = useRef();
 
-    const handleJoin = (e)=> {
-        e.preventDefault()
-        emailjs.sendForm(
-            'service_extzqa9',
-            'template_5fbt3fr',
-            form.current,
-            'VLwg1ltOWvnCYAiK_'
-        )
-        .then((result)=>{
-            console.log('done')
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_9d8lnsm', 'template_m9bwg3s', form.current, {
+        publicKey: 'R1wt5zF8Z1esx47u1',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
         },
-        (error)=>{
-            console.log(error)
-        }
-        )
-    }
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+    
   return (
    <div className="Join" id='join-us'>
     <div className="left-j">
@@ -29,9 +31,13 @@ const Join = () => {
         <div><span>your body</span><span className='stroke-text'>with us?</span></div>
     </div>
     <div className="right-j">
-        <form className="email-container" ref={form} onSubmit={handleJoin}>
+        {/* <form className="email-container" ref={form} onSubmit={sendEmail}>
             <input type="email" name='user_email' placeholder='Enter your Email Address here...'/>
             <button type='submit' className="btn btn-j">Join now</button>
+        </form> */}
+        <form ref={form} className=' email-container' onSubmit={sendEmail}>
+            <input type="email" name='user_email' placeholder='Enter Your Email Address'/>
+            <button className='btn btn-j'>Join Now</button>
         </form>
     </div>
    </div>
